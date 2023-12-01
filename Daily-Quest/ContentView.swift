9d0881736f1185905eed5d99e3038c689b3888e3
Patrick Lanham <p.lanham91@icloud.com>
2023-12-01 11:33:39 +0100

    Initial Commit

---
+++ Daily-Quest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Daily-Quest
+//
+//  Created by Patrick Lanham on 01.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Daily-Quest/Daily_QuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_QuestApp.swift
+//  Daily-Quest
+//
+//  Created by Patrick Lanham on 01.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Daily_QuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
